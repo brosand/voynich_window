ba7e4574fb7f2f88a9424fafce899e84f77bb778
brosand <brosand@gmail.com>
2019-12-02 19:08:30 -0500

    War and peace comparison setup

--- voynich_base.py
+++ voynich_base.py
@@ -44,8 +44,26 @@ def parse_line(line):
         parsed = list(parsed) + ([''] * (line_type - 2))
     return parsed, line_type
 
+def convert_to_strings(big_text, line_count=5000):
+    with open(big_text) as corpus:
+        paragraphs = []
+        line = corpus.readline().rstrip('\n').rstrip('.')
+        cur_paragraph = []
+        i=0
+        while line and i<line_count:
+            cur_paragraph = cur_paragraph + line.split(' ')
+            line = corpus.readline()
+            if not line or line == '\n':
+                paragraphs.append(cur_paragraph)
+                cur_paragraph = []
+            if line != '\n':
+                line = line.rstrip('\n').rstrip('.')
+        i += 1
+        return paragraphs
 
-def convert_to_strings(df):
+
+
+def convert_to_strings_voynich(df):
     i = 0
     paragraph = ''
     output = []
@@ -66,16 +84,17 @@ def convert_to_strings(df):
             # paragraph += line['Line']
             # TODO these are all different
         i += 1
-    for paragraph in output:
-        i = 0
-        n = 10
-        paragraph.replace('<->', '.')
-        paragraph.replace('<$>', '')
-        paragraph = paragraph.split('.')
+    for k, paragraph in enumerate(output):
+
+        output[k] = paragraph.replace('<->', '.')
+        output[k] = paragraph.replace('<\\$>', '')
+        output[k] = paragraph.split('.')
  
     return output
 
 def get_levenshtein(pair):
+    if not (pair[0].isalpha() and pair[1].isalpha()):
+        return -1
     seq1, seq2 = pair
     size_x = len(seq1) + 1
     size_y = len(seq2) + 1
@@ -133,18 +152,20 @@ def create_df(file, hand):
     A_df = df[df['H'] == '1']
     B_df = df[df['H'] == '2']
     if hand=='A':
-        str_list_output = convert_to_strings(A_df)
+        str_list_output = convert_to_strings_voynich(A_df)
     if hand=='B':
-        str_list_output = convert_to_strings(B_df)
+        str_list_output = convert_to_strings_voynich(B_df)
     else:
-        str_list_output = convert_to_strings(df)
+        str_list_output = convert_to_strings_voynich(df)
     return str_list_output
 
 def gen_comps(str_list_output, neg_dist=1):
     word_comp = defaultdict(list)
     comp_count = defaultdict(lambda: 0)
     for paragraph in str_list_output:
-       while i < len(paragraph) - n:
+        i = 0
+        n = 10
+        while i < len(paragraph) - n:
             window = paragraph[i:i + n]
             word1 = window[0]
             for k, word_2 in enumerate(window[1:]):
@@ -159,7 +180,7 @@ def gen_comps(str_list_output, neg_dist=1):
                     word_comp[word1, word_2].append(k)
                     comp_count[word1,word_2] += 1
             i += 1
-        for k, word_2 in enumerate(paragraph[i:]):
+        for k, word1 in enumerate(paragraph[i:]):
             for m, word_2 in enumerate(paragraph[(i+k+1):]):
                 if comp_count[word1, word_2] == 0:
                     if comp_count[word_2, word1] == 0:
@@ -190,34 +211,36 @@ def gen_comps(str_list_output, neg_dist=1):
             #     word_comb[comb].append(dist)
     return word_comp, comp_count
 
-def analysis(word_comp, comp_count, n=1000):
+def analysis(word_comp, comp_count, n=10000):
 
     topitems = heapq.nlargest(n, comp_count.items(), key=itemgetter(1))
 
     topitemsdict = dict(topitems)
+    topitemsdict = {item: topitemsdict[item] for item in topitemsdict.keys() if (item[0].isalpha() and item[1].isalpha())}
     top_comps = {item: word_comp[item] for item in topitemsdict.keys()}
 
 
     pair = list(top_comps.keys())[0]
-    vis = top_comps[pair]
+    # vis = top_comps[pair]
 
     stdevs = {a:statistics.stdev(top_comps[a]) for a in top_comps.keys()}
     median_dist = {a:statistics.median(top_comps[a]) for a in top_comps.keys()}
 
-    lowest_stdevs = heapq.nsmallest(10, stdevs)
-    highest_stdevs = heapq.nlargest(10, stdevs)
+    # lowest_stdevs = heapq.nsmallest(10, stdevs)
+    # highest_stdevs = heapq.nlargest(10, stdevs)
     
 
-    m = min(stdevs, key=stdevs.get)
+    # m = min(stdevs, key=stdevs.get)
 
-    levenshteins = {item:get_levenshtein(item) for item in word_comp.keys()}
+    # levenshteins = {item:get_levenshtein(item) for item in word_comp.keys()}
+    levenshteins_top = {item:get_levenshtein(item) for item in topitemsdict.keys()}
 
-    x = [levenshteins[item] for item in stdevs.keys()]
-    y = [stdevs[item] for item in stdevs.keys()]
-    y1 = [comp_count[item] for item in stdevs.keys()]
-    y2 = [median_dist[item] for item in stdevs.keys()]
+    # x = [levenshteins[item] for item in stdevs.keys()]
+    # y = [stdevs[item] for item in stdevs.keys()]
+    # y1 = [comp_count[item] for item in stdevs.keys()]
+    # y2 = [median_dist[item] for item in stdevs.keys()]
 
-    return topitemsdict, stdevs, levenshteins
+    return topitemsdict, stdevs, levenshteins_top
 
 
 
@@ -229,6 +252,11 @@ def main(hand='Both'):
     # count_good = sum([c.isalnum() for paragraph in str_list_output for c in paragraph])
     word_comp, comp_count = gen_comps(str_list_output)
     topitems, stdevs, levenshteins = analysis(word_comp, comp_count)
+
+    war_peace_strs = convert_to_strings('war_peace_test.txt', 6000)
+    word_comp_eng, comp_count_eng = gen_comps(war_peace_strs)
+    # word_comp_eng = {a:word_comp_eng[a] if word_comp_eng[a] !=0 for a in word_comp_eng.keys()]
+    topitems_eng, stdevs_eng, levenshteins_eng = analysis(word_comp_eng, comp_count_eng)
 
     return topitems, comp_count, word_comp, stdevs, levenshteins
 
